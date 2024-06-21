447e615b2b4ecd9f13f6a01eb589cb01cb469731
ZeroMan2002 <toborobotic@gmail.com>
2024-06-21 05:27:53 +0100

    test5

--- bot.py
+++ bot.py
@@ -32,14 +32,38 @@ async def on_ready():
     if channel:
         await channel.send("Bot is now online!")
 
-if __name__ == "__main__":
-    from threading import Thread
-    def run_bot():
-        bot.run(DISCORD_TOKEN)
-    
-    Thread(target=run_bot).start()
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    # Use Gunicorn as the production server
+    import multiprocessing
+    workers = multiprocessing.cpu_count() * 2 + 1
+    bind = '0.0.0.0:5000'
+    reload = True if os.getenv('FLASK_ENV') == 'development' else False
+    loglevel = 'debug' if os.getenv('FLASK_ENV') == 'development' else 'info'
 
+    # Run Flask app with Gunicorn
+    from gunicorn.app.base import BaseApplication
+
+    class StandaloneApplication(BaseApplication):
+        def __init__(self, app, options=None):
+            self.options = options or {}
+            self.application = app
+            super().__init__()
+
+        def load_config(self):
+            for key, value in self.options.items():
+                self.cfg.set(key, value)
+
+        def load(self):
+            return self.application
+
+    options = {
+        'bind': bind,
+        'workers': workers,
+        'reload': reload,
+        'loglevel': loglevel
+    }
+
+    StandaloneApplication(app, options).run()
 
 @bot.event
 async def on_ready():
