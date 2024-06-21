b49f62e4896caa7d526f0c637a8edc4138282bc4
ZeroMan2002 <toborobotic@gmail.com>
2024-06-21 06:06:08 +0100

    restall

--- bot.py
+++ bot.py
@@ -16,17 +16,9 @@ async def on_ready():
     channel = bot.get_channel(CHANNEL_ID)
     if channel:
         print(f'Connected to channel: {channel.name} (ID: {channel.id})')
-
-@bot.command(name='force_online')
-async def force_online(ctx):
-    """Forces the bot to send an online message to the configured channel."""
-    channel = bot.get_channel(CHANNEL_ID)
-    if channel:
-        await channel.send("Force online command received. Bot is now online!")
+        await channel.send("Bot is now online!")
     else:
-        await ctx.send("Error: Channel not found. Check configuration.")
-
-# Add more bot commands and event handlers here as needed
+        print(f'Channel with ID {CHANNEL_ID} not found.')
 
 if __name__ == '__main__':
     bot.run(DISCORD_TOKEN)
